52e6d7744a4917b445fa89c505b169aa13603f68
scueee <1464632340@qq.com>
2020-09-22 02:19:20 +0800

    增加自动补全功能

--- README.md
+++ README.md
@@ -4,6 +4,24 @@
 
 鼠标指向在关键词后会弹出悬浮菜单，支持 MD ，可自定义关键词。
 
+输入`key`后会自动补全，任你选择各种关键词。
+
+悬浮菜单：
+
+![](img/1.png)
+
+自动补全1：
+
+![](img/4.png)
+
+自动补全2：
+
+![](img/2.png)
+
+右键菜单：
+
+![](img/3.png)
+
 ### 开始
 
 - `Ctrl+Shirft+P`或从右键菜单中打开**命令面板**。
@@ -20,7 +38,10 @@
 
 - 你同样可以在 vscode 设置中搜索 **Writer Helper: Path**,填入配置文件的路径。
 
-### 参考配置文件如下
+- 当你需要自动补全关键词时，请建立工作区，并将右键菜单**生成 Snippets** 生成的 `writer-helper.code-snippets` 文件移至 `.vscode`下。
+
+
+**参考配置文件如下**
 
 data.txt
 
@@ -45,4 +66,48 @@ data.txt
 [玄派人名生成器](https://www.xuanpai.com/tool/names)
 ```
 
+### 以下为一些可能会用到的 vscode 设置：
 
+#### 打字机模式
+
+`Editor: Cursor Surrounding Lines`
+控制光标周围可见的前置行和尾随行的最小数目。在其他一些编辑器中称为“scrollOff”或“scrollOffset”。
+
+#### 文本折叠
+
+`Editor: Folding`
+控制编辑器是否启用了代码折叠。
+
+#### 自动换行
+
+`Editor: Word Wrap`
+控制折行的方式。
+
+`Editor: Word Wrap Column`
+在 Editor: Word Wrap 为 wordWrapColumn 或 bounded 时，控制编辑器的折行列。
+
+#### 无干扰模式
+
+`Ctrl + K Z` 进入禅模式
+连按两次 ESC 退出
+
+#### 折行缩进从第一列开始
+
+`Editor: Wrapping Indent`
+控制折行的缩进。
+
+#### Tab 空两格
+
+`Editor: Tab Size`
+一个制表符等于的空格数。在 Editor: Detect Indentation 启用时，根据文件内容，该设置可能会被覆盖。
+
+`Editor: Detect Indentation`
+控制是否在打开文件时，基于文件内容自动检测 Editor: Tab Size 和 Editor: Insert Spaces。
+
+#### 自动保存
+
+`Files: Auto Save`
+控制自动保存未保存的编辑器。有关自动保存的详细信息，请参阅此处。
+
+`Files: Auto Save Delay`
+控制自动保存未保存的编辑器之前经过的延迟(以毫秒为单位)。仅当 Files: Auto Save 设置为afterDelay时才适用。
--- extension.js
+++ extension.js
@@ -7,16 +7,30 @@ const fs = require("fs");
 function activate(context) {
 
 	let disposable = vscode.commands.registerCommand('extension.load', function () {
-		vscode.window.showInformationMessage('WriterHelper 已启用');
+		vscode.window.setStatusBarMessage('WriterHelper 已启用',3000);
 	});
 	context.subscriptions.push(disposable);
 
 	var array = []; //该数组用于存放配置文件的每行文本
 	var path = "";
 
+	//生成 Snippets 文件
+	context.subscriptions.push(vscode.commands.registerCommand('extension.createSnippets', (uri) => {
+		path = vscode.workspace.getConfiguration().get('writerHelper.path');
+		fs.readFile(path, function (err, data) {
+			if (err) {
+				return console.error(err);
+			}
+			array = data.toString().split("\r\n");
+			createSnippets(array,uri.path.slice(1,uri.path.lastIndexOf('/')).toString().concat("/writer-helper.code-snippets"));
+		});
+		vscode.window.setStatusBarMessage('Snippets 已生成',3000);
+	}));
+
 	//获取当前文件路径，修改配置
 	context.subscriptions.push(vscode.commands.registerCommand('extension.getPath', (uri) => {
 		vscode.workspace.getConfiguration().update('writerHelper.path',uri.path.substring(1),true);
+		vscode.window.setStatusBarMessage('配置已写入',3000);
 	}));
 
 	//监听配置变化
@@ -64,4 +78,26 @@ exports.activate = activate;
 
 module.exports = {
 	activate
+}
+
+function createSnippets(array,path) {
+	var str2 = "";
+    for(var i=0;i<array.length;i++) {
+        if(array[i][0]=="@") {
+            str2 = str2.concat(array[i].substring(1)+',');
+        }
+    }
+    var str1 = '{"key": {"prefix": "key","body": ["${1|'
+    str2 = str2.slice(0,-1);
+    var str3 = '|}"],"description": "关键词"}}'
+
+    str1 = str1.concat(str2);
+    str1 = str1.concat(str3);
+
+	let str = JSON.stringify(JSON.parse(str1),null,"\t");
+    fs.writeFile(path,str,function(err){
+    if (err) {
+        return console.error(err);
+    }
+    })
 }
